47a3e56283f542cf8266a40b727d2d70e66fdbf2
“Breno <brenorodriguesdev@gmail.com>
2021-12-24 11:19:11 -0300

    docs: change title

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -4,7 +4,7 @@ import schemas from './schemas'
 export default {
   openapi: '3.0.0',
   info: {
-    title: 'Desafio Frexco API',
+    title: 'Desafio T10 API',
     version: '1.0.0'
   },
   servers: [{

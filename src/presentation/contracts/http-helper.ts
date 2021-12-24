35df95d571c9e0098051752e91c9f667133058cd
“Breno <brenorodriguesdev@gmail.com>
2021-12-24 10:26:37 -0300

    feat: add http helper

---
+++ src/presentation/contracts/http-helper.ts
@@ -0,0 +1,22 @@
+import { ServerError } from '../errors/server-error'
+import { HttpResponse } from './http'
+
+export const serverError = (): HttpResponse => ({
+  data: new ServerError(),
+  statusCode: 500
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  data: error,
+  statusCode: 400
+})
+
+export const ok = (data: any): HttpResponse => ({
+  data,
+  statusCode: 200
+})
+
+export const created = (): HttpResponse => ({
+  data: null,
+  statusCode: 201
+})

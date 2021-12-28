e6337a0a23003279ec4dbd0f8cbe1f29db4e6dfd
“Breno <brenorodriguesdev@gmail.com>
2021-12-28 06:41:46 -0300

    fix: participation entity

--- src/data/entities/participation.ts
+++ src/data/entities/participation.ts
@@ -1,16 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
-
-@Entity()
 export class Participation {
-  @PrimaryGeneratedColumn()
   id?: number
 
-  @Column()
   firstName: string
 
-  @Column()
   lastName: string
 
-  @Column()
   participation: number
 }

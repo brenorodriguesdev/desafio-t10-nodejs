dc29c5eb35a169431d8024e2b4cf2edaf67a8ace
“Breno <brenorodriguesdev@gmail.com>
2021-12-28 06:48:39 -0300

    feat: add get all participation factory

--- src/main/factories/controllers/get-all-participation.ts
+++ src/main/factories/controllers/get-all-participation.ts
@@ -1,10 +1,10 @@
-import { ParticipationRepositoryTypeORM } from '../../../infra/participation-repository'
 import { Controller } from '../../../presentation/contracts/controller'
 import { GetAllParticipationService } from '../../../data/services/get-all-participation'
 import { GetAllParticipationController } from '../../../presentation/controllers/get-all-participation'
+import { ParticipationRepositoryFirebase } from '../../../infra/participation-repository-firebase'
 
 export const makeGetAllParticipationController = (): Controller => {
-  const participationRepositoryTypeORM = new ParticipationRepositoryTypeORM()
-  const getAllParticipationService = new GetAllParticipationService(participationRepositoryTypeORM)
+  const participationRepositoryFirebase = new ParticipationRepositoryFirebase()
+  const getAllParticipationService = new GetAllParticipationService(participationRepositoryFirebase)
   return new GetAllParticipationController(getAllParticipationService)
 }
